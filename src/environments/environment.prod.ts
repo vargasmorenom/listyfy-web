3070e7327ee255a87cb9d7b247379dea1db37545
vargasmorenom <mc_vm@hotmail.com>
2026-04-20 14:40:20 -0500

    ajustes de conexion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   servicio: [
     {
-      url: 'https://backappv01-production.up.railway.app:8080/',
+      url: 'https://api-mylistys-production.up.railway.app/',
       key: 'encrypt!135790',
       logo: './../../../assets/logo/logoanchoNegro-2.png',
       logosmall: './../../../assets/logo/logoanchoNegro-small.png',
       logoHeader: 'assets/logo/logoMyllistys.png',
       logoHeaderInscription: 'assets/logo/logoMyllistys.png',
-      urlfiles: 'https://mylistys.backappv01-production.up.railway.app:8080/files/',
-      appUrl: 'mylistys.backappv01-production.up.railway.app:8080',
+      urlfiles: 'https://api-mylistys-production.up.railway.app/files/',
+      appUrl: 'https://api-mylistys-production.up.railway.app',
       termsUrl: 'https://mylistys.com/terminos',
       recaptchaSiteKey: '6Le9OpAsAAAAAEBabC1joHvAxIJQL5XV96qM-tyV',
-      recaptchaEnabled: false,
+      recaptchaEnabled: true,
       defaultAvatar: 'assets/logo/perfil02.png',
     },
   ],
