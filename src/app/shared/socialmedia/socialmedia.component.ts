b2b4859c63c2b72d873c72a58b370104434fd04c
vargasmorenom <mc_vm@hotmail.com>
2026-04-25 13:48:54 -0500

    compartir

--- src/app/shared/socialmedia/socialmedia.component.ts
+++ src/app/shared/socialmedia/socialmedia.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { logoFacebook, logoWhatsapp, logoTwitter } from 'ionicons/icons';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-socialmedia',
@@ -20,12 +21,14 @@ export class SocialmediaComponent {
     return this.contentCount > 0 && !!this.postId;
   }
 
+  private readonly backendUrl = environment.servicio[0].url.replace('/api/v1/', '');
+
   constructor() {
     addIcons({ logoFacebook, logoWhatsapp, logoTwitter });
   }
 
   private getShareUrl(): string {
-    return `${window.location.origin}/shared/${this.postId}`;
+    return `${this.backendUrl}/share/${this.postId}`;
   }
 
   private openShare(buildUrl: (url: string) => string): void {
